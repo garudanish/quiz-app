7cbd2882921b922fd0006b4a4d8b0fc692103e11
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-07-25 20:17:55 +0900

    Adjust Home component and create quiz component

---
+++ pages/quiz.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const Quiz = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>QUIZ</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Quiz;
